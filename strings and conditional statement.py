90d74dc1283552587f626c5a7655511878f577c4
Asmit_平和 <asmitpahan48@gmail.com>
2025-11-15 20:19:33 +0530

    Add files via upload
    “This program contains information and code related to strings and statements in Python.”

---
+++ strings and conditional statement.py
@@ -0,0 +1,22 @@
+
+
+
+# concatination adding 2 or more strings
+str1 = "asmit"
+num2 = "pahan.   "
+print(len(num2+str1))
+#length of a string
+#indexing
+print(num2[3])
+#cannot write num2[2]=dathipe means cant give index as varibale
+ 
+#slicing
+print(str1[0:6])#counting in positive wll be 0 to inf
+#counting in negative will be -inf to -1
+#string function
+
+print (num2.capitalize())
+print (num2.endswith("it"))
+# replace
+# find
+# count
