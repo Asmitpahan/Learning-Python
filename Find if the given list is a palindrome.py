fd297f1c70ee93a570bab319496677a8016bbea0
Asmit pahan <asmitpahan48@gmail.com>
2025-11-15 21:29:15 +0530

    Find palindrome

---
+++ Find if the given list is a palindrome.py
@@ -0,0 +1,13 @@
+List1=[]
+a =input("write 1 thing for list:")
+b =input("write 2 thing for list:")
+c =input("write 3 thing for list:")
+List1.append(a)
+List1.append(b)
+List1.append(c)
+List2 = List1.copy()
+List1.reverse()
+if List1 == List2 :
+    print("Its a palindrome")
+else :
+    print("its not a palindrome")
