e98308e024d5c0dd3e7d7bf445cb530f3af10603
Asmit pahan <asmitpahan48@gmail.com>
2025-11-15 21:39:49 +0530

    Added a another way i think

--- Find if the given list is a palindrome.py
+++ Find if the given list is a palindrome.py
@@ -11,3 +11,17 @@ if List1 == List2 :
     print("Its a palindrome")
 else :
     print("its not a palindrome")
+#-------------------------------------
+# another way i think
+List1=[]
+List2 =[]
+List2 == List1
+a =input("write 1 thing for list:")
+b =input("write 2 thing for list:")
+c =input("write 3 thing for list:")
+List1.append(a)
+List1.append(b)
+List1.append(c)
+List1.reverse()
+if List1==List2:
+    print("Its a palindrome")
