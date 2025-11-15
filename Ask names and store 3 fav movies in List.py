24d8fc2ace57ea1c0efb5e48b34f830ddc932578
Asmit pahan <asmitpahan48@gmail.com>
2025-11-15 21:15:38 +0530

    code done

--- Ask names and store 3 fav movies in List.py
+++ Ask names and store 3 fav movies in List.py
@@ -3,3 +3,14 @@ b = input("WHATS YOUR FAVORITE MOVIE person 2:")
 c = input("WHATS YOUR FAVORITE MOVIE person 3:")
 Listz = [a,b,c]
 print(Listz)
+
+#------------------------------------------------
+#just another way to write it
+movies = []
+mov1 = input("WHATS YOUR FAVORITE MOVIE person 1:")
+mov2 = input("WHATS YOUR FAVORITE MOVIE person 2:")
+mov3 = input("WHATS YOUR FAVORITE MOVIE person 3:")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
